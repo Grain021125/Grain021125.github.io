d099722292fd64941ef7f07a42ef3c295989ded0
Grain021125 <576656893@qq.com>
2026-03-15 16:43:39 +0800

    Site updated: 2026-03-15 16:43:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/14/TinyCoro项目-1-io-uring异步IO/","2026/03/15/TinyCoro项目-3-tinyCoroLab-1/","2026/03/14/TinyCoro项目-2-tinyCoro介绍/","2026/03/13/hello-world/"];function toRandomPost(){
+var posts=["2026/03/14/TinyCoro项目-2-tinyCoro介绍/","2026/03/14/TinyCoro项目-1-io-uring异步IO/","2026/03/13/hello-world/","2026/03/15/TinyCoro项目-3-tinyCoroLab-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
